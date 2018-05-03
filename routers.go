f4e641dc21e653a25444a33a8b6a775bd3f184a0
caoziguang <caoziguang@bytedance.com>
2018-05-03 14:53:36 +0800

    init

---
+++ routers.go
@@ -0,0 +1,31 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+//GetPattern is the map of urls with GET method
+var GetPattern = map[string]gin.HandlerFunc{
+	"/ping": Ping,
+}
+
+//PostPattern is the map of urls with POST method
+var PostPattern = map[string]gin.HandlerFunc{}
+
+//BothPattern is the map of urls with GET&POST method
+var BothPattern = map[string]gin.HandlerFunc{}
+
+//SetRouters set how to handle with request url
+func SetRouters(r *gin.Engine) {
+
+	for path, f := range GetPattern {
+		r.GET(path, f)
+	}
+
+	for path, f := range PostPattern {
+		r.POST(path, f)
+	}
+
+	for path, f := range BothPattern {
+		r.GET(path, f)
+		r.POST(path, f)
+	}
+}
